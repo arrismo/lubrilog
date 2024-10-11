d8e0ad3854b3cfa631bba578d6d5cbbbc4ba05b1
arrismo <mikaelmoise00@gmail.com>
2024-10-11 17:06:12 -0400

    added test for mdy

--- tests/testthat/test-mdy.R
+++ tests/testthat/test-mdy.R
@@ -1,3 +1,22 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(lubrilog)
+library(cli)
+
+data <- lubrilog::games
+
+
+f <- function(){
+  lubrilog::mdy(data$release_date)
+}
+
+test_that("mdy works", {
+  expect_message(
+    out <- lubrilog::mdy(data$release_date)
+  )
+  expect_equal(length(out), nrow(data))
+
+  expect_message(f(), "NA values created by parsing")
+  expect_message(f(), "Original:")
+  expect_message(f(), "Parsed:")
 })
+
+
