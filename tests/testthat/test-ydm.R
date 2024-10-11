6e4b3fe743b9ca7803958aa618cbb52a5e65e14a
arrismo <mikaelmoise00@gmail.com>
2024-10-11 17:08:12 -0400

    adding more tests

--- tests/testthat/test-ydm.R
+++ tests/testthat/test-ydm.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(lubrilog)
+library(cli)
+
+data <- lubrilog::games
+
+
+f <- function(){
+  lubrilog::ydm(data$release_date)
+}
+
+test_that("ydm works", {
+  expect_message(
+    out <- lubrilog::ydm(data$release_date)
+  )
+  expect_equal(length(out), nrow(data))
+
+  expect_message(f(), "NA values created by parsing")
+  expect_message(f(), "Original:")
+  expect_message(f(), "Parsed:")
 })
+
--- tests/testthat/test-year.R
+++ tests/testthat/test-year.R
@@ -1,3 +1,21 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(lubrilog)
+library(cli)
+
+data <- lubrilog::games
+
+
+f <- function(){
+  lubrilog::year(data$release_date)
+}
+
+test_that("year works", {
+  expect_message(
+    out <- lubrilog::year(data$release_date)
+  )
+  expect_equal(length(out), nrow(data))
+
+  expect_message(f(), "NA values created by parsing")
+  expect_message(f(), "Original:")
+  expect_message(f(), "Parsed:")
 })
+
